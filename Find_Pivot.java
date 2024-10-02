3c3d44d6e66e306cbad82245fce21ef987fab396
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2024-10-02 22:02:52 +0530

    done Roatational Sorted array

--- Find_Pivot.java
+++ Find_Pivot.java
@@ -3,11 +3,17 @@ public class Find_Pivot {
     public static void main(String[] args) {
         
         int[] arr = {3,4,5,6,7,0,1,2};
+        // int []arr = {0,1,2,3,4,5,6,7};
         int pivotIndex = findPivot(arr);
         System.out.println(" index is : "+pivotIndex);
-        System.out.println("Index element is : "+arr[pivotIndex]);
+        
+        if(isRotational_Sorted_Array(arr)){
+            System.out.println("Not Rotational Sorted Array ");
+        }
+        else{
+            System.out.println("Rotational Sorted array");
+        }
     }
-
     static int findPivot(int[] arr){
 
         int start = 0;
@@ -34,11 +40,41 @@ public class Find_Pivot {
                 end = mid-1;
             }
 
+
+
         }
         return -1;  // Pivot is not found array is not rotated 
 
     }
 
+    static boolean isRotational_Sorted_Array(int[] arr){
 
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<= end){
+
+            int mid = start +(end-start)/2;
+
+            // check if pivot is middle element :
+            if(mid <end && arr[mid]>arr[mid+1]){
+                return false;
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
+                return false;
+            }
+
+            // decide which half is choose for Binary Search : 
+            if(arr[mid] >=arr[start]){
+                start= mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+
+        }
+
+        return true;
+    }
 }
 
