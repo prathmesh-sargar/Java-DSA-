0a21b02ee59af8e04e201aec86bc7bc22da184dc
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2024-10-08 23:24:24 +0530

    Patterns Questions

---
+++ Print_Pattern.java
@@ -0,0 +1,64 @@
+public class Print_Pattern {
+
+    public static void main(String[] args) {
+
+        pattern4(5);
+    }
+
+    static void pattern1(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+
+        for (int i = 0; i <n; i++) {
+
+            for (int space = 0; space < n - i; space++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void pattern4(int n ){
+
+        int maxval =n;
+          n=2*n;
+        for (int i = 0; i <=n; i++) {
+
+            for (int j = 0; j <=n; j++) {
+
+                // this is important formula to understand it .. 
+                int atEveryIndex = maxval- Math.min(Math.min(i,j) , Math.min(n-i,n-j));
+
+                System.out.print(atEveryIndex+ "  ");
+            }
+            System.out.println();
+        }
+    }
+         
+
+
+
+}
